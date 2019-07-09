e507d763393a6d1e1cd699ecf79fbd0dea2c54f3
R. S. Doiel <rsdoiel@gmail.com>
2019-07-09 12:46:00 -0700

    fixed MathThing.Mod

--- oberon.bash
+++ oberon.bash
@@ -5,7 +5,12 @@ if [[ ! -f "$DISC_IMAGE" && "$1" = "" ]]; then
     echo "Copying DiskImage/Oberon-2019-01-21.dsk ${DISC_IMAGE}"
     cp -vi DiskImage/Oberon-2019-01-21.dsk "${DISC_IMAGE}"
 fi
+if [[ ! -f risc ]]; then
+    echo "Building the risc emulator"
+    make
+fi
 if [[ -f "${DISC_IMAGE}" ]]; then
+    echo "Running Oberon with $DISC_IMAGE"
     ./risc --mem 2048 --size 1025x512 "${DISC_IMAGE}"
 else
     echo "Can't find $DISC_IMAGE";
